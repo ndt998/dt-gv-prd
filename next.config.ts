bb03a111006ddb4b4a9e1573ed0562dc71b6e3f5
ndt998 <nguyenduytan16798@gmail.com>
2026-04-19 22:01:01 +0700

    clean up and prepare for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  /* config options here */
+  /* Đã xóa dòng output: "export" ở đây */
   typescript: {
     ignoreBuildErrors: true,
   },
