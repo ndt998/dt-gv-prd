75b874847c7a95f0a0833ec90b268aa0d852bfa4
ndt998 <nguyenduytan16798@gmail.com>
2026-06-19 11:01:27 +0700

    Update page.tsx
    Đổi hệ thống đánh giá

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ const menuItems: MenuItem[] = [
     id: 'stats',
     title: 'Phân tích chất lượng tiết giảng',
     icon: <BarChart3 className="w-5 h-5" />,
-    link: 'https://script.google.com/macros/s/AKfycbwgEATIeK--zMY8UswuZO2G7it4OJ6gHE-6gR_0nMGQt87cpwya78kSRvGH1ubrzbs4/exec'// Đang cập nhật
+    link: 'https://script.google.com/macros/s/AKfycbxmL_OssCFpC6Qzu3M6Y9qMxcnv215fRr3nNxeAQWWM2XkAYuSph25KfWSkCSSlbhHc-A/exec'// Đang cập nhật
   },
   {
     id: 'progress',
